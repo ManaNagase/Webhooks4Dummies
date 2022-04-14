05c120ad7ccb6121afd9d643750e3415bea581ac
Mana <88516116+ManaNagase@users.noreply.github.com>
2022-04-14 13:24:54 +0200

    Fixin'

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 import os
 import sys
-os.system("pip install discord")
-os.system("pip install requests")
-os.system("pip install discord_webhook")
-os.system("pip install time")
+try:
+    import discord
+except:
+    os.system("pip install discord")
+try:
+    import requests
+except:
+    os.system("pip install requests")
+try:
+    import discord_webhook
+except:
+    os.system("pip install discord_webhook")
+
 sys.exit()
